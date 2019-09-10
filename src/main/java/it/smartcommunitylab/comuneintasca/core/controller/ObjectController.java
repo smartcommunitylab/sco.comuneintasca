73efd05c644dbe8c00d9e02f0b8acf8b17667856
kazhamiakin <kazhamiakin@gmail.com>
2019-09-10 13:58:31 +0200

    Expose highlights

--- src/main/java/it/smartcommunitylab/comuneintasca/core/controller/ObjectController.java
+++ src/main/java/it/smartcommunitylab/comuneintasca/core/controller/ObjectController.java
@@ -1,13 +1,6 @@
 package it.smartcommunitylab.comuneintasca.core.controller;
 
-import it.smartcommunitylab.comuneintasca.core.model.BaseCITObject;
-import it.smartcommunitylab.comuneintasca.core.model.EventObject;
-import it.smartcommunitylab.comuneintasca.core.model.HotelObject;
-import it.smartcommunitylab.comuneintasca.core.model.ItineraryObject;
-import it.smartcommunitylab.comuneintasca.core.model.ObjectFilter;
-import it.smartcommunitylab.comuneintasca.core.model.POIObject;
-import it.smartcommunitylab.comuneintasca.core.model.RestaurantObject;
-
+import java.util.Collections;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
@@ -17,9 +10,30 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import it.smartcommunitylab.comuneintasca.core.model.BaseCITObject;
+import it.smartcommunitylab.comuneintasca.core.model.DynamicConfigObject;
+import it.smartcommunitylab.comuneintasca.core.model.EventObject;
+import it.smartcommunitylab.comuneintasca.core.model.HotelObject;
+import it.smartcommunitylab.comuneintasca.core.model.ItineraryObject;
+import it.smartcommunitylab.comuneintasca.core.model.MenuItem;
+import it.smartcommunitylab.comuneintasca.core.model.ObjectFilter;
+import it.smartcommunitylab.comuneintasca.core.model.POIObject;
+import it.smartcommunitylab.comuneintasca.core.model.RestaurantObject;
 
 @Controller
 public class ObjectController extends AbstractObjectController {
+
+	@RequestMapping(method = RequestMethod.GET, value = "/highlights/{appId}")
+	public ResponseEntity<?> highlights(@PathVariable String appId, @RequestParam(required=false) String profile) throws Exception{
+		List<DynamicConfigObject> result = storage.searchObjects(appId, DynamicConfigObject.class, null, null, null, null, null, null, null, 1, 0);
+		if (result != null && result.size() > 0) {
+			DynamicConfigObject conf = result.get(0);
+			return new ResponseEntity<List<MenuItem>>(conf.getHighlights(), HttpStatus.OK);
+		}
+		 return new ResponseEntity<List<MenuItem>>(Collections.<MenuItem>emptyList(),HttpStatus.OK);
+	}
 
 	@RequestMapping(method = RequestMethod.POST, value = "/events/{appId}")
 	public ResponseEntity<?> events(@PathVariable String appId, @RequestBody ObjectFilter filter) throws Exception{
